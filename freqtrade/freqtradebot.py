cb3a97570576352b9624a4a7f40eb031b947eed6
Bob <buaaxlr@126.com>
2025-03-25 18:00:00 +0800

    fix code rebuild

--- freqtrade/freqtradebot.py
+++ freqtrade/freqtradebot.py
@@ -10,6 +10,7 @@ from math import isclose
 from threading import Lock
 from time import sleep
 from typing import Any
+from itertools import product
 
 from schedule import Scheduler
 
@@ -307,52 +308,55 @@ class FreqtradeBot(LoggingMixin):
         self.last_process = datetime.now(timezone.utc)
 
     def check_unknown_trades(self, pair_whitelist) -> None:
-        if self.config.get("dual_side", False):
-            cnt = 0
-            positions = self.wallets.get_all_positions()
-            for pair in pair_whitelist:
-                for is_short in [False, True]:
-                    side = "short" if is_short else "long"
-                    if pos := positions.get(pair+"_" + side):
-                        trades = Trade.get_open_trades_with_pair_side_opendate(pair = pair,is_short = is_short)
-                        if len(trades) > 0:
-                            trade = trades[0]
-                            if trades.leverage != pos.leverage:
-                                cnt += 1
-                                trades.leverage = pos.leverage
-                                Trade.session.refresh(trade)
-                        else:
-                            trade = Trade(
-                                    pair=pair,
-                                    base_currency=self.exchange.get_pair_base_currency(pair),
-                                    stake_currency=self.config["stake_currency"],
-                                    stake_amount=0,
-                                    amount=0,
-                                    is_open=True,
-                                    amount_requested=pos.position,
-                                    fee_open=0,
-                                    fee_close=0,
-                                    open_rate=pos.entry_price,
-                                    open_rate_requested=pos.entry_price,
-                                    open_date=pos.dt,
-                                    exchange=self.exchange.id,
-                                    strategy=self.strategy.get_strategy_name(),
-                                    enter_tag='手动',
-                                    timeframe=timeframe_to_minutes(self.config["timeframe"]),
-                                    leverage=pos.leverage,
-                                    is_short=is_short,
-                                    trading_mode=self.trading_mode,
-                                    funding_fees=0,
-                                    amount_precision=self.exchange.get_precision_amount(pair),
-                                    price_precision=self.exchange.get_precision_price(pair),
-                                    precision_mode=self.exchange.precisionMode,
-                                    precision_mode_price=self.exchange.precision_mode_price,
-                                    contract_size=1.0,
-                                )
-                            cnt += 1
-                            Trade.session.add(trade)
-            if cnt > 0:
-                Trade.commit()
+        if not self.config.get("dual_side", False):
+            return None
+        cnt = 0
+        positions = self.wallets.get_all_positions()
+        for pair, is_short in product(pair_whitelist, [False, True]):
+            side = "short" if is_short else "long"
+            pos = positions.get(pair+"_" + side)
+            if pos is None:
+                continue
+
+            trades = Trade.get_open_trades_with_pair_side_opendate(pair = pair,is_short = is_short)
+            if len(trades) > 0:
+                trade = trades[0]
+                if trades.leverage != pos.leverage:
+                    cnt += 1
+                    trades.leverage = pos.leverage
+                    Trade.session.refresh(trade)
+            else:
+                trade = Trade(
+                    pair=pair,
+                    base_currency=self.exchange.get_pair_base_currency(pair),
+                    stake_currency=self.config["stake_currency"],
+                    stake_amount=0,
+                    amount=0,
+                    is_open=True,
+                    amount_requested=pos.position,
+                    fee_open=0,
+                    fee_close=0,
+                    open_rate=pos.entry_price,
+                    open_rate_requested=pos.entry_price,
+                    open_date=pos.dt,
+                    exchange=self.exchange.id,
+                    strategy=self.strategy.get_strategy_name(),
+                    enter_tag='手动',
+                    timeframe=timeframe_to_minutes(self.config["timeframe"]),
+                    leverage=pos.leverage,
+                    is_short=is_short,
+                    trading_mode=self.trading_mode,
+                    funding_fees=0,
+                    amount_precision=self.exchange.get_precision_amount(pair),
+                    price_precision=self.exchange.get_precision_price(pair),
+                    precision_mode=self.exchange.precisionMode,
+                    precision_mode_price=self.exchange.precision_mode_price,
+                    contract_size=1.0,
+                )
+                cnt += 1
+                Trade.session.add(trade)
+        if cnt > 0:
+            Trade.commit()
         return None
 
 
